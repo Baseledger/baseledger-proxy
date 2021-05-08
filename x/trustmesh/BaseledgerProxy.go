4281a885a83ea4b0bfd0fe98985f00f5f3d99dcd
Marten <marten.jung@jung-schmidt.de>
2021-05-08 22:16:22 +0200

    Added BaseledgerPRoxy as implementation of IBasledgerPRoxyInterface

---
+++ x/trustmesh/BaseledgerProxy.go
@@ -0,0 +1,26 @@
+package trustmesh
+
+//Implements IBaseledgerInterface from /baseledger/x/trustmesh/types/types.go
+
+var (
+//global vars
+)
+
+const (
+//global constants
+)
+
+func init() {
+	//anything to initialize?
+	/*userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}*/
+
+}
+
+//Implements IBaseledgerInterface from /baseledger/x/trustmesh/types/types.go
+//privatize(text string) string
+//deprivatize(textEncrypted string) string
+
+//hash(payload string) string
