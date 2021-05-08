de4827f6e0d57670cae12035b9160b5f669b1706
Marten <marten.jung@jung-schmidt.de>
2021-05-08 16:02:08 +0200

    added first typ in trustmesh module - put all proxy types here?

--- x/trustmesh/types/types.go
+++ x/trustmesh/types/types.go
@@ -1,1 +1,17 @@
 package types
+
+//Put here our Types needed for the proxy elements?
+type OffchainProcessMessageReferenceType string
+
+type OffchainProcessMessage struct {
+	senderID                         string
+	receiverID                       string
+	topic                            string
+	offchainProcessMessageID         string
+	referencedOffchainProcessMessage string
+	referenceType                    OffchainProcessMessageReferenceType
+	//BusinessObject todo
+	hash                                 string
+	blockchainTransactionIdOfStoredProof string
+	baseledgerTransactionIdOfStoredProof string
+}
