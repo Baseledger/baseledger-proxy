bea454b6ab98ffbd11face99e1397d81e9a5d86a
Stefan Kostic <skostic9242@gmail.com>
2021-06-28 11:36:50 +0200

    Move init db methods to dbutil

--- app/app.go
+++ app/app.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"database/sql"
 	"fmt"
 	"io"
 	"net/http"
@@ -100,11 +99,8 @@ import (
 	"github.com/unibrightio/baseledger/x/proxy/messaging"
 	proxytypes "github.com/unibrightio/baseledger/x/proxy/types"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres" // postgres
 
-	"github.com/golang-migrate/migrate"
-	"github.com/golang-migrate/migrate/database/postgres"
 	_ "github.com/golang-migrate/migrate/source/file"
 
 	"github.com/spf13/viper"
@@ -182,94 +178,6 @@ func init() {
 	}
 
 	DefaultNodeHome = filepath.Join(userHomeDir, "."+Name)
-}
-
-func initDbIfNotExists() {
-	dbHost, _ := viper.Get("DB_HOST").(string)
-	dbSuperUser, _ := viper.Get("DB_UB_USER").(string)
-	dbPwd, _ := viper.Get("DB_UB_PWD").(string)
-	dbDefaultName, _ := viper.Get("DB_UB_NAME").(string)
-	sslMode, _ := viper.Get("DB_SSLMODE").(string)
-	dbUser, _ := viper.Get("DB_BASELEDGER_USER").(string)
-	dbName, _ := viper.Get("DB_BASELEDGER_NAME").(string)
-
-	args := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s sslmode=%s",
-		dbHost,
-		dbSuperUser,
-		dbPwd,
-		dbDefaultName,
-		sslMode,
-	)
-	db, err := gorm.Open("postgres", args)
-
-	if err != nil {
-		fmt.Printf("db connection failed %v\n", err.Error())
-		panic(err)
-	}
-
-	fmt.Printf("admin db connection successful")
-
-	exists := db.Exec(fmt.Sprintf("select 1 from pg_roles where rolname='%s'", dbUser))
-
-	if exists.RowsAffected == 1 {
-		fmt.Printf("row already exits")
-		return
-	}
-
-	result := db.Exec(fmt.Sprintf("create user %s with superuser password '%s'", dbUser, dbPwd))
-
-	if result.Error != nil {
-		fmt.Printf("failed to create user %v\n", result.Error)
-		panic(result.Error)
-	}
-
-	result = db.Exec(fmt.Sprintf("create database %s owner %s", dbName, dbUser))
-
-	if result.Error != nil {
-		fmt.Printf("failed to create baseledger db %v\n", result.Error)
-		panic(result.Error)
-	}
-
-	db.DB().Close() // Close admin connection TODO: Move whole section above to dbutil
-}
-
-func performMigrations() {
-	dbHost, _ := viper.Get("DB_HOST").(string)
-	dbPwd, _ := viper.Get("DB_UB_PWD").(string)
-	sslMode, _ := viper.Get("DB_SSLMODE").(string)
-	dbUser, _ := viper.Get("DB_BASELEDGER_USER").(string)
-	dbName, _ := viper.Get("DB_BASELEDGER_NAME").(string)
-
-	dsn := fmt.Sprintf("postgres://%s/%s?user=%s&password=%s&sslmode=%s",
-		dbHost,
-		dbName,
-		dbUser,
-		dbPwd,
-		sslMode,
-	)
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		fmt.Printf("migrations failed 1: %s", err.Error())
-		panic(err)
-	}
-
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
-	if err != nil {
-		fmt.Printf("migrations failed 2: %s", err.Error())
-		panic(err)
-	}
-
-	m, err := migrate.NewWithDatabaseInstance("file://./ops/migrations", dbName, driver)
-	if err != nil {
-		fmt.Printf("migrations failed 3: %s", err.Error())
-		panic(err)
-	}
-
-	err = m.Up()
-	if err != nil && err != migrate.ErrNoChange {
-		fmt.Printf("migrations failed 4: %s", err.Error())
-	}
 }
 
 func subscribeToWorkgroupMessages() {
@@ -587,10 +495,10 @@ func New(
 	viper.AddConfigPath("../")
 	viper.SetConfigFile(".env")
 
-	initDbIfNotExists()
-	performMigrations()
-
+	dbutil.InitDbIfNotExists()
+	dbutil.PerformMigrations()
 	dbutil.InitConnection()
+
 	subscribeToWorkgroupMessages()
 
 	cron.StartCron()
