b327cacba02268d199bcec9d6ddcbc5597f10aaa
Stefan Kostic <skostic9242@gmail.com>
2021-07-05 21:27:56 +0200

    Add trigger for setting trustmesh id before insert new trustmesh entry

--- ops/migrations/000001_initial.down.sql
+++ ops/migrations/000001_initial.down.sql
@@ -1,15 +1,17 @@
+ALTER TABLE ONLY public.offchain_process_messages DROP CONSTRAINT offchain_process_messagess_pkey;
+ALTER TABLE ONLY public.trustmesh_entries DROP CONSTRAINT trustmesh_entries_pkey;
+ALTER TABLE ONLY public.trustmesh_entries DROP CONSTRAINT trustmesh_entries_offchain_process_message_id_offchain_process_messages_id_foreign;
+
+ALTER TABLE ONLY public.trustmesh_entries ALTER COLUMN offchain_process_message_id TYPE text;
+ALTER TABLE ONLY public.trustmesh_entries DROP CONSTRAINT trustmesh_entries_trustmesh_id_trustmeshes_id_foreign;
+ALTER TABLE ONLY public.trustmeshes DROP CONSTRAINT trustmeshes_pkey;
+
+DROP FUNCTION IF EXISTS set_trustmesh_entry_group CASCADE;
+DROP TRIGGER IF EXISTS trustmesh_entry_insert_trigger ON trustmesh_entries;
+
 DROP TABLE public.trustmesh_entries;
+DROP TABLE public.offchain_process_messages;
 DROP TABLE public.workgroup_members;
 DROP TABLE public.workgroups;
 DROP TABLE public.organizations;
-
-ALTER TABLE ONLY public.offchain_process_messages DROP CONSTRAINT offchain_process_messagess_pkey;
-
-DROP TABLE public.offchain_process_messages;
-
-ALTER TABLE ONLY public.trustmesh_entries DROP CONSTRAINT trustmesh_entries_pkey;
-
-ALTER TABLE ONLY public.trustmesh_entries DROP CONSTRAINT trustmesh_entries_offchain_process_message_id_offchain_process_messages_id_foreign;
-
-ALTER TABLE ONLY public.trustmesh_entries
-    ALTER COLUMN offchain_process_message_id TYPE text;
+DROP TABLE public.trustmeshes;
--- ops/migrations/000001_initial.up.sql
+++ ops/migrations/000001_initial.up.sql
@@ -16,8 +16,8 @@ COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UU
 -- TODO: add PK and FK constraints
 
 CREATE TABLE public.organizations (
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    organization_name text NOT NULL
+  id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+  organization_name text NOT NULL
 );
 
 ALTER TABLE public.organizations OWNER TO baseledger;
@@ -26,8 +26,8 @@ INSERT INTO public.organizations (organization_name)
 VALUES ('Org1'), ('Org2');
 
 CREATE TABLE public.workgroups (
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    workgroup_name text NOT NULL
+  id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+  workgroup_name text NOT NULL
 );
 
 ALTER TABLE public.workgroups OWNER TO baseledger;
@@ -36,11 +36,11 @@ INSERT INTO public.workgroups (workgroup_name)
 VALUES ('Workgroup1');
 
 CREATE TABLE public.workgroup_members (
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    workgroup_id uuid NOT NULL,
-    organization_id uuid NOT NULL,
-    organization_endpoint text NOT NULL,
-    organization_token text NOT NULL
+  id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+  workgroup_id uuid NOT NULL,
+  organization_id uuid NOT NULL,
+  organization_endpoint text NOT NULL,
+  organization_token text NOT NULL
 );
 
 ALTER TABLE public.workgroup_members OWNER TO baseledger;
@@ -71,42 +71,54 @@ WITH
   select w.id, o.id, 'host.docker.internal:4223', 'testToken1'
   from w, o;
 
+CREATE TABLE public.trustmeshes (
+  id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+  created_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+ALTER TABLE public.trustmeshes OWNER TO baseledger;
+ALTER TABLE ONLY public.trustmeshes ADD CONSTRAINT trustmeshes_pkey PRIMARY KEY (id);
+
 CREATE TABLE public.trustmesh_entries (
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    tendermint_block_id text,
-    tendermint_transaction_id uuid,
-    tendermint_transaction_timestamp timestamp with time zone,
-    
-    entry_type text,
+  id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+  tendermint_block_id text,
+  tendermint_transaction_id uuid,
+  tendermint_transaction_timestamp timestamp with time zone,
+  
+  entry_type text,
 
-    sender_org_id uuid,
-    receiver_org_id uuid,
-    workgroup_id uuid,
+  sender_org_id uuid,
+  receiver_org_id uuid,
+  workgroup_id uuid,
 
-    workstep_type text,
-    baseledger_transaction_type text,
+  workstep_type text,
+  baseledger_transaction_type text,
 
-    baseledger_transaction_id uuid,
-    referenced_baseledger_transaction_id uuid,
+  baseledger_transaction_id uuid,
+  referenced_baseledger_transaction_id uuid,
 
-    business_object_type text,
-    baseledger_business_object_id uuid,
-    referenced_baseledger_business_object_id uuid,
+  business_object_type text,
+  baseledger_business_object_id uuid,
+  referenced_baseledger_business_object_id uuid,
 
-    offchain_process_message_id uuid,
-    referenced_process_message_id uuid,
+  offchain_process_message_id uuid,
+  referenced_process_message_id uuid,
 
-    commitment_state text,
-    transaction_hash text
+  commitment_state text,
+  transaction_hash text,
+  trustmesh_id uuid NOT NULL
 );
 
 ALTER TABLE public.trustmesh_entries OWNER TO baseledger;
 
 CREATE INDEX idx_commitment_state ON public.trustmesh_entries USING btree (commitment_state);
 
+ALTER TABLE ONLY public.trustmesh_entries
+  ADD CONSTRAINT trustmesh_entries_trustmesh_id_trustmeshes_id_foreign FOREIGN KEY (trustmesh_id) REFERENCES public.trustmeshes(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
 CREATE TABLE public.offchain_process_messages (
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    sender_id uuid,
+  id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+  sender_id uuid,
 	receiver_id uuid,
 	topic text,
 	referenced_offchain_process_message_id uuid,
@@ -115,10 +127,10 @@ CREATE TABLE public.offchain_process_messages (
 	business_object_proof text,
 	tendermint_transaction_id_of_stored_proof uuid,
 	baseledger_transaction_id_of_stored_proof uuid,
-    baseledger_business_object_id uuid,
+  baseledger_business_object_id uuid,
 	referenced_baseledger_business_object_id uuid,
 	status_text_message text,
-    business_object_type text,
+  business_object_type text,
 	baseledger_transaction_type text,
 	referenced_baseledger_transaction_id uuid,
 	entry_type text
@@ -131,7 +143,29 @@ ALTER TABLE ONLY public.offchain_process_messages ADD CONSTRAINT offchain_proces
 ALTER TABLE ONLY public.trustmesh_entries ADD CONSTRAINT trustmesh_entries_pkey PRIMARY KEY (id);
 
 ALTER TABLE ONLY public.trustmesh_entries
-    ALTER COLUMN offchain_process_message_id TYPE uuid USING (uuid_generate_v4());
+  ALTER COLUMN offchain_process_message_id TYPE uuid USING (uuid_generate_v4());
 
 ALTER TABLE ONLY public.trustmesh_entries
-    ADD CONSTRAINT trustmesh_entries_offchain_process_message_id_offchain_process_messages_id_foreign FOREIGN KEY (offchain_process_message_id) REFERENCES public.offchain_process_messages(id) ON UPDATE CASCADE ON DELETE CASCADE;
+  ADD CONSTRAINT trustmesh_entries_offchain_process_message_id_offchain_process_messages_id_foreign FOREIGN KEY (offchain_process_message_id) REFERENCES public.offchain_process_messages(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+CREATE OR REPLACE FUNCTION set_trustmesh_entry_group()
+   RETURNS trigger AS
+ $$
+ DECLARE new_trustmesh_id uuid;
+ BEGIN
+ 	IF NEW.referenced_baseledger_transaction_id = uuid_nil() THEN
+ 		INSERT INTO trustmeshes VALUES (DEFAULT, DEFAULT) RETURNING id INTO new_trustmesh_id;
+	ELSE 
+		SELECT trustmesh_id INTO new_trustmesh_id FROM trustmesh_entries WHERE baseledger_transaction_id = NEW.referenced_baseledger_transaction_id;
+	END IF;
+	NEW.trustmesh_id := new_trustmesh_id;
+ RETURN NEW;
+ END;
+ $$
+ LANGUAGE plpgsql;
+
+ CREATE TRIGGER trustmesh_entry_insert_trigger
+   BEFORE INSERT
+   ON trustmesh_entries
+   FOR EACH ROW
+   EXECUTE PROCEDURE set_trustmesh_entry_group();
