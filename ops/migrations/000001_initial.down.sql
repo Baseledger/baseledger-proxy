bbfa18162cec9d471a71058738d1880d379a27c6
Stefan Kostic <skostic9242@gmail.com>
2021-07-08 02:29:19 +0200

    Fix migrations

--- ops/migrations/000001_initial.down.sql
+++ ops/migrations/000001_initial.down.sql
@@ -1,17 +1,18 @@
-ALTER TABLE ONLY public.offchain_process_messages DROP CONSTRAINT offchain_process_messagess_pkey;
-ALTER TABLE ONLY public.trustmesh_entries DROP CONSTRAINT trustmesh_entries_pkey;
+DROP INDEX idx_commitment_state;
+
+
 ALTER TABLE ONLY public.trustmesh_entries DROP CONSTRAINT trustmesh_entries_offchain_process_message_id_offchain_process_messages_id_foreign;
 
-ALTER TABLE ONLY public.trustmesh_entries ALTER COLUMN offchain_process_message_id TYPE text;
+ALTER TABLE ONLY public.offchain_process_messages DROP CONSTRAINT offchain_process_messages_pkey;
+ALTER TABLE ONLY public.trustmesh_entries DROP CONSTRAINT trustmesh_entries_pkey;
 ALTER TABLE ONLY public.trustmesh_entries DROP CONSTRAINT trustmesh_entries_trustmesh_id_trustmeshes_id_foreign;
 ALTER TABLE ONLY public.trustmeshes DROP CONSTRAINT trustmeshes_pkey;
 
 DROP FUNCTION IF EXISTS set_trustmesh_entry_group CASCADE;
-DROP TRIGGER IF EXISTS trustmesh_entry_insert_trigger ON trustmesh_entries;
 
+DROP TABLE public.workgroups;
+DROP TABLE public.workgroup_members;
 DROP TABLE public.trustmesh_entries;
 DROP TABLE public.offchain_process_messages;
-DROP TABLE public.workgroup_members;
-DROP TABLE public.workgroups;
 DROP TABLE public.organizations;
 DROP TABLE public.trustmeshes;
--- ops/migrations/000001_initial.up.sql
+++ ops/migrations/000001_initial.up.sql
@@ -138,12 +138,9 @@ CREATE TABLE public.offchain_process_messages (
 
 ALTER TABLE public.offchain_process_messages OWNER TO baseledger;
 
-ALTER TABLE ONLY public.offchain_process_messages ADD CONSTRAINT offchain_process_messagess_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.offchain_process_messages ADD CONSTRAINT offchain_process_messages_pkey PRIMARY KEY (id);
 
 ALTER TABLE ONLY public.trustmesh_entries ADD CONSTRAINT trustmesh_entries_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY public.trustmesh_entries
-  ALTER COLUMN offchain_process_message_id TYPE uuid USING (uuid_generate_v4());
 
 ALTER TABLE ONLY public.trustmesh_entries
   ADD CONSTRAINT trustmesh_entries_offchain_process_message_id_offchain_process_messages_id_foreign FOREIGN KEY (offchain_process_message_id) REFERENCES public.offchain_process_messages(id) ON UPDATE CASCADE ON DELETE CASCADE;
