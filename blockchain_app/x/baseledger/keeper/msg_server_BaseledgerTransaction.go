83200cb33fb38be2a0d25ca5331598b5050d12cd
Stefan Kostic <skostic9242@gmail.com>
2021-07-30 01:31:36 +0200

    Print module account and tx creator account in keeper create method

--- blockchain_app/x/baseledger/keeper/msg_server_BaseledgerTransaction.go
+++ blockchain_app/x/baseledger/keeper/msg_server_BaseledgerTransaction.go
@@ -6,12 +6,23 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/tendermint/tendermint/crypto"
 	"github.com/unibrightio/baseledger/x/baseledger/types"
 )
 
 func (k msgServer) CreateBaseledgerTransaction(goCtx context.Context, msg *types.MsgCreateBaseledgerTransaction) (*types.MsgCreateBaseledgerTransactionResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
+	moduleAcct := sdk.AccAddress(crypto.AddressHash([]byte(types.ModuleName)))
+	fmt.Printf("BASELEDGER MODULE ACCOUNT %v %v\n", moduleAcct, moduleAcct.String())
+
+	txCreatorAddress, err := sdk.AccAddressFromBech32(msg.Creator)
+
+	fmt.Printf("TX CREATOR ACCOUNT %v %v\n", txCreatorAddress, txCreatorAddress.String())
+
+	if err != nil {
+		panic(err)
+	}
 	var BaseledgerTransaction = types.BaseledgerTransaction{
 		Id:                      msg.Id,
 		Creator:                 msg.Creator,
