94c0d2a8def826045536fb428397c7f305444c69
Ognjen Kurtic <ognjen.kurtic@finspot.rs>
2021-08-19 14:06:33 +0200

    ...

--- ops/testnet/add-node-to-running-blokchain.sh
+++ ops/testnet/add-node-to-running-blokchain.sh
@@ -1,10 +1,10 @@
 first_node_tendermint_p2p_port=26655
 tendermint_internal_grpc_port=26657
-node1_id=c4fe0042d61f9fb8eec3db08ea31a4f998d1045c
-node1_ip=46.163.115.138
+node1_id=fa9e205e2591309888374fe6f1e931e92a7352e1
+node1_ip=46.163.116.182
 
-docker exec baseledger-node_blockchain_app_1 baseledgerd init node55 --chain-id baseledger
-docker exec baseledger-node_blockchain_app_1 baseledgerd keys add node55_replicator --keyring-backend test
+docker exec baseledger-node_blockchain_app_1 baseledgerd init <new_name> --chain-id baseledger
+docker exec baseledger-node_blockchain_app_1 baseledgerd keys add <new_name_role> --keyring-backend test
 docker exec baseledger-node_blockchain_app_1 sed -i 's/persistent_peers = ""/persistent_peers = "'${node1_id}'@'${node1_ip}':'${first_node_tendermint_p2p_port}'"/' ~/.baseledger/config/config.toml
 docker exec baseledger-node_blockchain_app_1 sed -i 's/addr_book_strict = true/addr_book_strict = false/' ~/.baseledger/config/config.toml
 docker exec baseledger-node_blockchain_app_1 sed -i 's/allow_duplicate_ip = false/allow_duplicate_ip = true/' ~/.baseledger/config/config.toml
