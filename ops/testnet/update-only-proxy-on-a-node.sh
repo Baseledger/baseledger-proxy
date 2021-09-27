bcd6ac5e0ba13d4d134f85e3fa00409da75b04f9
Ognjen Kurtic <ognjen.kurtic@finspot.rs>
2021-09-27 08:47:30 +0200

    update docs

--- ops/testnet/update-only-proxy-on-a-node.sh
+++ ops/testnet/update-only-proxy-on-a-node.sh
@@ -1,10 +1,16 @@
-node 1	
+#local
+
+#navigate to repo root/proxy_app
+docker build -f ops\Dockerfile -t baseledger/proxy_app .
+docker push baseledger/proxy_app
+
+# node 1	
 docker stop <proxy_container>
 docker rm <proxy_container>
 docker rmi <proxy_image>
 docker run --name=baseledger-node_proxy_app_1 --restart=always --network=baseledger-node_baseledger -p 8081:8080 --add-host=host.docker.internal:host-gateway -e DB_HOST=postgres-local-node -e ORGANIZATION_ID=d9c102eb-b173-45ac-b640-24d28a3c9f0c -e API_CONCIRCLE_URL=s4h.rp.concircle.com -d baseledger/proxy_app
 
-node 2
+# node 2
 docker stop <proxy_container>
 docker rm <proxy_container>
 docker rmi <proxy_image>
